08424888a01160ca69562d2108ba3fca35f1bedf
JLRaffaele <battery.dc2@hotmail.com>
2018-03-01 18:10:10 -0800

    A pin will now be placed at each zipcode in the database. Collision not handled

--- app/src/main/java/com/raffaele/jeanluc/playerlocator/MapActivity.java
+++ app/src/main/java/com/raffaele/jeanluc/playerlocator/MapActivity.java
@@ -1,12 +1,15 @@
 package com.raffaele.jeanluc.playerlocator;
 
 import android.content.SharedPreferences;
+import android.graphics.Point;
 import android.location.Address;
 import android.location.Geocoder;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.PreferenceManager;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -16,8 +19,20 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Jean-Luc on 1/24/2018.
@@ -26,52 +41,149 @@ import java.util.List;
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 {
     String myzip = "";
-    double latitude, longitude;
+    LatLng myLatLng;
+    List<String> Usernames = new ArrayList<>();
+    List<LatLng> LatLngs = new ArrayList<>();
+
+    ConnectionClass connectionClass;
+
+    Boolean connectionSuccess;
+    final Geocoder geocoder = new Geocoder(this);
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-        final Geocoder geocoder = new Geocoder(this);
+
+        connectionClass = new ConnectionClass();
+        GetMapData mapData = new GetMapData();
+
+            mapData.execute("");
+
         try
         {
+            //get user zipcode from preferences
             SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
-
             myzip = sharedPref.getString("zipcode_preference", "");
+
             List<Address> addresses = geocoder.getFromLocationName(myzip, 1);
+
             if (addresses != null && !addresses.isEmpty())
             {
                 Address address = addresses.get(0);
-                latitude = address.getLatitude();
-                longitude = address.getLongitude();
+                myLatLng = new LatLng(address.getLatitude(), address.getLongitude());
+
+
             }
             else
             {
                 Toast.makeText(MapActivity.this, "Unable to geocode zipcode", Toast.LENGTH_LONG).show();
+
             }
+
+
         }
         catch (IOException e)
         {
-            Log.d("preftest", "error in getting lat and long");
+            Log.d("mapError", "error in getting lat and long");
         }
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+            super.onCreate(savedInstanceState);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+            setContentView(R.layout.activity_maps);
+            final SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+
+            mapFragment.getMapAsync(this);
+
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-
-        LatLng userLocation = new LatLng(latitude, longitude);
+    public void onMapReady(GoogleMap googleMap)
+    {
         float zoom = 10;
-        googleMap.addMarker(new MarkerOptions().position(userLocation)
+        googleMap.addMarker(new MarkerOptions().position(myLatLng)
                 .title("Your Location!"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, zoom));
+
+        //add marker for each user
+        for (int i = 0; i < LatLngs.size(); i++)
+        {
+            LatLng userLocation = LatLngs.get(i);
+
+            googleMap.addMarker(new MarkerOptions().position(userLocation)
+                    .title(Usernames.get(i)));
+        }
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLatLng, zoom));
+
 
     }
+
+
+    public class GetMapData extends AsyncTask<String,String,String>
+    {
+        String z = "";
+
+
+
+        @Override
+        protected String doInBackground(String... params)
+        {
+            String userZip;
+            String userName;
+            connectionSuccess = false;
+
+
+            try
+            {
+                Connection conn = connectionClass.CONN();
+
+                if (conn == null)
+                    z = "could not populate map";
+                else
+                {
+                    //Get username and zipcode from database
+                    String query = "SELECT loc,username FROM UserInfo join Users on UserInfo.id=Users.id";
+
+                    Statement stmnt = conn.createStatement();
+                    ResultSet rs = stmnt.executeQuery(query);
+
+                    SharedPreferences sharedPref = getSharedPreferences("PlayerLocator", MODE_PRIVATE);
+                    String currentUser = sharedPref.getString("username", "UNKNOWN");
+
+                    while (rs.next())
+                    {
+                        userZip = rs.getString("loc");
+                        userName = rs.getString("username");
+                        List<Address> addresses = geocoder.getFromLocationName(userZip, 1);
+                        Boolean validResult = !(userName.equals(currentUser));
+                        if (addresses != null && !addresses.isEmpty() && validResult)
+                        {
+
+                            Address address = addresses.get(0);
+                            LatLngs.add(new LatLng(address.getLatitude(), address.getLongitude()));
+                            Usernames.add(userName);
+                        }
+                    }
+                    z = "Got map data";
+                    connectionSuccess= true;
+                }
+            }
+
+            catch(Exception ex)
+            {
+
+                z = "Exceptions";
+            }
+
+            return z;
+        }
+
+        @Override
+        protected void onPostExecute(String types)
+        {
+
+        }
+    }
+
 
 }
